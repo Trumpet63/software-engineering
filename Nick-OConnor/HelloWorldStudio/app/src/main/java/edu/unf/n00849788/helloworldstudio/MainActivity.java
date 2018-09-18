c0b5529d2877078e1e4028c2b15706404caa6059
Nick <Nick>
2018-09-17 23:49:42 -0400

    Added my Hello World Apps for Android Studio and React Native

---
+++ Nick-OConnor/HelloWorldStudio/app/src/main/java/edu/unf/n00849788/helloworldstudio/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.unf.n00849788.helloworldstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
