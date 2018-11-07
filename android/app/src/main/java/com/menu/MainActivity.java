d0b8c8f1e4ebe6df1b68df524333ce47553fbebc
Nick <Nick>
2018-11-07 18:57:13 -0500

    Added android and ios folder as they are needed to avoid project rebuild

--- android/app/src/main/java/com/menu/MainActivity.java
+++ android/app/src/main/java/com/menu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.menu;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,23 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Menu";
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideNavigationBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+
+    private void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
 }
