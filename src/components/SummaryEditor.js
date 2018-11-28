6b9969293274841df92498d661d21089c0c364d7
Nick <Nick>
2018-11-28 09:48:55 -0500

    removed max for summarys per jeff

--- src/components/SummaryEditor.js
+++ src/components/SummaryEditor.js
@@ -14,93 +14,89 @@ class SummaryEditor extends Component {
   }
 
   addToMealSummary = () => {
-    if (this.restaurant.Summary.length < 3) {
-      this.props.dispatch({
-        type: 'AddToSummary',
-        Restaurant: this.restaurant,
-        SummaryObject: this.props.summaryObject,
-      });
-      this.props.parent.setState({
-        selectedItem: {},
-        text: '',
-      });
-    }
-    else {
-      alert("Your meal summary is already full...");
-    }
-  }
-
-  removeFromMealSummary = () => {
     this.props.dispatch({
-      type: 'RemoveFromSummary',
+      type: 'AddToSummary',
       Restaurant: this.restaurant,
       SummaryObject: this.props.summaryObject,
     });
     this.props.parent.setState({
       selectedItem: {},
+      text: '',
     });
   }
+}
 
-  updateMealSummary = () => {
-    this.props.dispatch({
-      type: 'UpdateSummaryObject',
-      Restaurant: this.restaurant,
-      SummaryObject: this.props.summaryObject,
-    });
-    this.props.parent.setState({
-      selectedItem: {},
-    });
-  }
+removeFromMealSummary = () => {
+  this.props.dispatch({
+    type: 'RemoveFromSummary',
+    Restaurant: this.restaurant,
+    SummaryObject: this.props.summaryObject,
+  });
+  this.props.parent.setState({
+    selectedItem: {},
+  });
+}
 
-  renderButton() {
-    if (this.props.buttonType == 'remove') {
-      return (
-        <View style={styles.URView}>
-          <Button title='Remove' color='red' onPress={this.removeFromMealSummary} />
-          <Button title='Update' onPress={this.updateMealSummary} />
-        </View>
-      );
-    }
-    else {
-      return (
-        <View style={styles.add_remove}>
-          <Button title='Add' onPress={this.addToMealSummary} />
-        </View>
-      );
-    }
-  }
+updateMealSummary = () => {
+  this.props.dispatch({
+    type: 'UpdateSummaryObject',
+    Restaurant: this.restaurant,
+    SummaryObject: this.props.summaryObject,
+  });
+  this.props.parent.setState({
+    selectedItem: {},
+  });
+}
 
-  render() {
+renderButton() {
+  if (this.props.buttonType == 'remove') {
     return (
-      <View style={styles.FirstContainer}>
-        <View style={styles.add_remove}>
-          {this.renderButton()}
-        </View>
-        <View style={styles.dropzone}>
-          <Image style={styles.Image}
-            source={{ uri: this.props.summaryObject.meal.image }} />
-        </View>
-        <View style={styles.portionControl}>
-          <Text>{Math.round(this.props.summaryObject.value * 100) + '%'} </Text>
-          <Slider minimumValue={0.5}
-            maximumValue={1.5}
-            value={this.props.summaryObject.value}
-            step={0.1}
-            onValueChange={(value) => this.setState({ value: value })}
-            onSlidingComplete={(value) => this.props.parent.setState({ selectedItem: { ...this.props.parent.state.selectedItem, value: value } })}
-            style={{ width: 100 }}
-          />
-        </View>
-        <View style={styles.liveDisplay}>
-          <Text>Meal: {this.props.summaryObject.meal.name}</Text>
-          <Text>Calories: {Math.round(this.props.summaryObject.meal.calories * this.props.summaryObject.value)}</Text>
-          <Text>Protein: {Math.round(this.props.summaryObject.meal.protein * this.props.summaryObject.value)}</Text>
-          <Text>Fat: {Math.round(this.props.summaryObject.meal.fat * this.props.summaryObject.value)}</Text>
-          <Text>Carbs: {Math.round(this.props.summaryObject.meal.carbs * this.props.summaryObject.value)}</Text>
-        </View>
+      <View style={styles.URView}>
+        <Button title='Remove' color='red' onPress={this.removeFromMealSummary} />
+        <Button title='Update' onPress={this.updateMealSummary} />
       </View>
     );
   }
+  else {
+    return (
+      <View style={styles.add_remove}>
+        <Button title='Add' onPress={this.addToMealSummary} />
+      </View>
+    );
+  }
+}
+
+render() {
+  return (
+    <View style={styles.FirstContainer}>
+      <View style={styles.add_remove}>
+        {this.renderButton()}
+      </View>
+      <View style={styles.dropzone}>
+        <Image style={styles.Image}
+          source={{ uri: this.props.summaryObject.meal.image }} />
+      </View>
+      <View style={styles.portionControl}>
+        <Text>{Math.round(this.props.summaryObject.value * 100) + '%'} </Text>
+        <Slider minimumValue={0.5}
+          maximumValue={1.5}
+          value={this.props.summaryObject.value}
+          step={0.1}
+          onValueChange={(value) => this.setState({ value: value })}
+          onSlidingComplete={(value) => this.props.parent.setState({ selectedItem: { ...this.props.parent.state.selectedItem, value: value } })}
+          style={{ width: 100 }}
+        />
+      </View>
+      <View style={styles.liveDisplay}>
+        <Text>Meal: {this.props.summaryObject.meal.name}</Text>
+        <Text>Calories: {Math.round(this.props.summaryObject.meal.calories * this.props.summaryObject.value)}</Text>
+        <Text>Protein: {Math.round(this.props.summaryObject.meal.protein * this.props.summaryObject.value)}</Text>
+        <Text>Fat: {Math.round(this.props.summaryObject.meal.fat * this.props.summaryObject.value)}</Text>
+        <Text>Carbs: {Math.round(this.props.summaryObject.meal.carbs * this.props.summaryObject.value)}</Text>
+      </View>
+    </View>
+  );
+}
 }
 let Window = Dimensions.get('window');
 const styles = StyleSheet.create({
